427f5dbee41db10f0b69e553e1e2bb3bda0564a6
jdrake16 <stovetops111@gmail.com>
2020-07-04 14:21:08 -0600

    Initial commit

---
+++ app/src/main/java/edu/cnm/deepdive/navigation/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.cnm.deepdive.navigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.cnm.deepdive.navigation.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main_activity);
+
+    }
+  }
+
